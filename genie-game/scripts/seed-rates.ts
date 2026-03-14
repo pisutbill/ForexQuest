a9733242ac45e7e8d531355591ef70ab4163fefc
pisutbill <pp76@st-andrews.ac.uk>
2026-03-14 11:43:00 +0000

    npx create script

--- genie-game/scripts/seed-rates.ts
+++ genie-game/scripts/seed-rates.ts
@@ -1,7 +1,9 @@
 import "dotenv/config";
 import { PrismaClient } from "../src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter });
 
 // Years to collect
 const START_YEAR = 2000;
